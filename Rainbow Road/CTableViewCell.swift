a867dca82a26977ee71181e3b75beca19d1563af
Hassan Al-Shammari <hsn999hsn@gmail.com>
2021-12-20 14:02:10 +0000

    New Project

---
+++ Rainbow Road/CTableViewCell.swift
@@ -0,0 +1,25 @@
+//
+//  CTableViewCell.swift
+//  Rainbow Road
+//
+//  Created by administrator on 06/12/2021.
+//
+
+import UIKit
+
+class CTableViewCell: UITableViewCell {
+
+    @IBOutlet weak var colorView: UIView!
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
--- Rainbow Road/ViewController.swift
+++ Rainbow Road/ViewController.swift
@@ -9,11 +9,27 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    let colors = [UIColor.red, UIColor.orange, UIColor.yellow, UIColor.green, UIColor.blue, UIColor.purple]
+    @IBOutlet weak var tableView: UITableView!
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        tableView.dataSource = self
     }
 
 
 }
 
+extension ViewController: UITableViewDataSource, UITableViewDelegate{
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        colors.count
+    }
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "cell", for: indexPath) as! CTableViewCell
+        cell.colorView.backgroundColor = colors[indexPath.row]
+        return cell
+    }
+    
+    
+    
+}
